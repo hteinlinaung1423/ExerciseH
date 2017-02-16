0bd91473a933e3c8c4df254c9bb427239931cccf
e0167351 <e0167351@DESKTOP-TQJDBEQ>
2017-02-15 18:07:14 -0800

    Add project files.

---
+++ ExerciseF/exeh1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseH
+{
+    class exeh1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+
+            string s1 = Console.ReadLine();
+            s1=s1.ToUpper();
+           
+
+            Console.Write("Enter a word :");
+
+            string s2 = Console.ReadLine();
+            s2=s2.ToUpper();
+
+            bool eql = InString(s1, s2);
+
+            if (eql)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+
+                Console.WriteLine("False");
+
+            }
+
+        }
+
+        public static bool InString(string s1, string s2)
+        {
+            bool eql = false;
+
+
+
+            
+
+            for (int i = 0; i <= s1.Length-s2.Length; i++)
+            {
+                string a = s1.Substring(i, s2.Length);
+                
+                
+                if (a == s2)
+                {
+                    eql = true;
+                    break;
+                }
+            }
+
+
+            return eql;
+
+
+
+        }
+
+    }
+}
---
+++ ExerciseF/exeh2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseH
+{
+    class exeh2
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string : ");
+
+            string s1 = Console.ReadLine();
+            s1 = s1.ToUpper();
+
+
+            Console.Write("Enter a word :");
+
+            string s2 = Console.ReadLine();
+            s2 = s2.ToUpper();
+
+            int eql = InString(s1, s2);
+
+            if (eql==-1)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+
+                Console.WriteLine(eql);
+
+            }
+
+        }
+
+        public static int InString(string s1, string s2)
+        {
+            int eql = -1;
+
+
+
+
+
+            for (int i = 0; i <= s1.Length - s2.Length; i++)
+            {
+                string a = s1.Substring(i, s2.Length);
+
+
+                if (a == s2)
+                {
+                    eql = i;
+                    break;
+                }
+            }
+
+
+            return eql;
+
+    }
+    }
+}
